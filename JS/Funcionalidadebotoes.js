16cf3afc00d25dafcf6fd89aaa87255d07b94a8f
JosiasNorte <josiasnorte@redejmu.local>
2022-05-25 16:33:49 -0300

    3 semestre original

---
+++ JS/Funcionalidadebotoes.js
@@ -0,0 +1,14 @@
+<scriipt>
+    function redirecionamentodepaginaSobreMim() {
+        window.location.href = "SobreMim.html"
+    }
+    function paginaProjetoIntegrador() {
+        window.location.href = "ProjetoIntegrador.html"
+    }
+    function paginaCompetências() {
+        window.location.href = "Competencias.html"
+    }
+    function paginaAulas() {
+        window.location.href = "Aulas.html"
+    }
+</scriipt>
